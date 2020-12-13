fa9aa99186a22ab42908eb99832b2fe7a167ff49
sdpheeing <63859974@qq.com>
2020-12-14 00:44:19 +0800

    1

--- app/common/repositories/system/merchant/MerchantIntentionRepository.php
+++ app/common/repositories/system/merchant/MerchantIntentionRepository.php
@@ -46,11 +46,10 @@ class MerchantIntentionRepository extends BaseRepository
         $form = Elm::createForm(Route::buildUrl('systemMerchantIntentionStatus', ['id' => $id])->build());
         $form->setRule([
             [
-	"type" => "image",
+	"type" => "img",
 	"field" => "image",
 	"title" => "图片",
 	"props" => [
-        "type" => "image",
 		"placeholder" => "请输入图片",
 		"src" => "http:\/\/apis.taoben888.cn\/uploads\/def\/20201204\/bcbc0d8a0841d7c024b2d3e901403aa6.gif",
 		"disabled" => true
