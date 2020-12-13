d1dc7189f2356564896d34e2b50fb31e6838ee32
sdpheeing <63859974@qq.com>
2020-12-13 18:45:51 +0800

    1

--- app/common/dao/user/UserDao.php
+++ app/common/dao/user/UserDao.php
@@ -115,6 +115,10 @@ class UserDao extends BaseDao
         return $query;
     }
 
+    public function save($where, $data)
+    {
+        return User::getDB()->where($where)->update($data);
+    }
     /**
      * @param $keyword
      * @return BaseQuery
