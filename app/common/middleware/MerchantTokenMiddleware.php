f9dc5d0f1e7836f97da64a716f127fedba73a16b
sdpheeing <63859974@qq.com>
2020-12-20 20:42:56 +0800

    1

--- app/common/middleware/MerchantTokenMiddleware.php
+++ app/common/middleware/MerchantTokenMiddleware.php
@@ -61,7 +61,7 @@ class MerchantTokenMiddleware extends BaseMiddleware
             }
 
 
-
+            dump($payload->jti[1]);die();
             if ('user' == $payload->jti[1]){
                 $user = $repository->get($payload->jti[0]);
                 $mer_id = app('UserMerRepository')->getUseridByMerid($user->uid);
