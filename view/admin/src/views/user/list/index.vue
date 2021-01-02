36ff6d1a89f77f3fb1abacc87721224ef6eb57da
luoyi <784554184@qq.com>
2021-01-02 18:11:45 +0800

    feat:发版

--- view/admin/src/views/user/list/index.vue
+++ view/admin/src/views/user/list/index.vue
@@ -203,9 +203,9 @@
             </el-table-column>
             <el-table-column label="状态" min-width="100">
                 <template slot-scope="{row}">
-                    <el-tag v-if="row.status == 1" type="success">通过</el-tag>
-                    <el-tag v-if="row.status == 0" type="info">未处理</el-tag>
-                    <el-tag v-if="row.status == 2" type="warning">未通过</el-tag>
+                    <el-tag v-if="row.user_ca == 1" type="success">通过</el-tag>
+                    <el-tag v-if="row.user_ca == 0" type="info">未处理</el-tag>
+                    <el-tag v-if="row.user_ca == 2" type="warning">未通过</el-tag>
                 </template>
             </el-table-column>
             <el-table-column label="推荐人" min-width="140">
