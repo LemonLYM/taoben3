598130cef528d0baa5fcca7d5eadb75f11932379
luoyi <784554184@qq.com>
2021-01-24 22:50:36 +0800

    feat:以旧换新

--- view/admin/src/views/product/productExamine/index.vue
+++ view/admin/src/views/product/productExamine/index.vue
@@ -134,21 +134,31 @@
           min-width="90"
         />
         <el-table-column
+          v-if="tableFrom.type !== 8"
           prop="sales"
           label="销量"
           min-width="90"
         />
         <el-table-column
+          v-if="tableFrom.type !== 8"
           prop="stock"
           label="库存"
           min-width="90"
         />
         <el-table-column
+          v-if="tableFrom.type !== 8"
           prop="sort"
           label="排序"
           min-width="70"
         />
         <el-table-column
+          v-if="tableFrom.type === 8"
+          prop="store_info"
+          label="以旧换新商品描述"
+          min-width="300"
+        />
+        <el-table-column
+          v-if="tableFrom.type !== 8"
           prop="status"
           label="是否显示"
           min-width="100"
@@ -233,11 +243,16 @@ import {
   merSelectApi,
   productOffApi,
   toVirtualSalesApi
-} from '@/api/product'
-import { roterPre } from '@/settings'
-import infoFrom from './info'
-import ueditorFrom from '@/components/ueditorFrom'
-const proOptions = [{ name: '热门榜单', value: 'is_hot' }, { name: '促销单品', value: 'is_benefit' }, { name: '精品推荐', value: 'is_best' }, { name: '首发新品', value: 'is_new' }]
+} from '@/api/product';
+import { roterPre } from '@/settings';
+import infoFrom from './info';
+import ueditorFrom from '@/components/ueditorFrom';
+const proOptions = [
+  { name: '热门榜单', value: 'is_hot' },
+  { name: '促销单品', value: 'is_benefit' },
+  { name: '精品推荐', value: 'is_best' },
+  { name: '首发新品', value: 'is_new' }
+];
 export default {
   name: 'ProductExamine1',
   components: { infoFrom, ueditorFrom },
@@ -248,7 +263,7 @@ export default {
       },
       ruleValidate: {
         store_name: [
-          { required: true, message: "请输入商品名称", trigger: "blur" },
+          { required: true, message: '请输入商品名称', trigger: 'blur' }
         ]
       },
       dialogVisible: false,
@@ -288,93 +303,110 @@ export default {
       merSelect: [],
       OffId: [],
       productId: 0
-    }
+    };
   },
   mounted() {
-    this.getMerSelect()
-    this.getList()
-    this.getCategorySelect()
-    this.getLstFilterApi()
+    this.getMerSelect();
+    this.getList();
+    this.getCategorySelect();
+    this.getLstFilterApi();
   },
   methods: {
     subSuccess() {
-      this.getList()
-      this.getLstFilterApi()
+      this.getList();
+      this.getLstFilterApi();
     },
     onchangeIsShow(row) {
-      changeApi(row.product_id, row.is_used).then(({ message }) => {
-        this.$message.success(message)
-        this.getList()
-      }).catch(({ message }) => {
-        this.$message.error(message)
-      })
+      changeApi(row.product_id, row.is_used)
+        .then(({ message }) => {
+          this.$message.success(message);
+          this.getList();
+        })
+        .catch(({ message }) => {
+          this.$message.error(message);
+        });
     },
     getInfo(id) {
-      this.fullscreenLoading = true
+      this.fullscreenLoading = true;
       this.checkboxGroup = [];
-      productDetailApi(id).then(res => {
-        const info = res.data
-        this.formValidate = {
-          is_hot: info.is_hot,
-          is_best: info.is_best,
-          is_new: info.is_new,
-          is_benefit: info.is_benefit,
-          ficti: info.ficti,
-          content: info.content,
-          store_name: info.store_name
-        }
-        if (info.is_benefit === 1) this.checkboxGroup.push('is_benefit')
-        if (info.is_hot === 1) this.checkboxGroup.push('is_hot')
-        if (info.is_best === 1) this.checkboxGroup.push('is_best')
-        if (info.is_new === 1) this.checkboxGroup.push('is_new')
-        this.fullscreenLoading = false
-        console.log(this.formValidate)
-      }).catch(res => {
-        this.$message.error(res.message)
-        this.fullscreenLoading = false
-      })
+      productDetailApi(id)
+        .then(res => {
+          const info = res.data;
+          this.formValidate = {
+            is_hot: info.is_hot,
+            is_best: info.is_best,
+            is_new: info.is_new,
+            is_benefit: info.is_benefit,
+            ficti: info.ficti,
+            content: info.content,
+            store_name: info.store_name
+          };
+          if (info.is_benefit === 1) this.checkboxGroup.push('is_benefit');
+          if (info.is_hot === 1) this.checkboxGroup.push('is_hot');
+          if (info.is_best === 1) this.checkboxGroup.push('is_best');
+          if (info.is_new === 1) this.checkboxGroup.push('is_new');
+          this.fullscreenLoading = false;
+          console.log(this.formValidate);
+        })
+        .catch(res => {
+          this.$message.error(res.message);
+          this.fullscreenLoading = false;
+        });
     },
     onEdit(id) {
-      this.productId = id
-      this.getInfo(id)
-      this.dialogVisible = true
+      this.productId = id;
+      this.getInfo(id);
+      this.dialogVisible = true;
     },
     // 编辑虚拟销量
     toVirtualSales(id) {
-      this.$modalForm(toVirtualSalesApi(id)).then(() => this.getList(''))
+      this.$modalForm(toVirtualSalesApi(id)).then(() => this.getList(''));
     },
     // 提交
     handleSubmit(name) {
-      console.log(this.formValidate)
-      this.$refs[name].validate((valid) => {
+      console.log(this.formValidate);
+      this.$refs[name].validate(valid => {
         if (valid) {
-          productUpdateApi(this.productId, this.formValidate).then(async res => {
-            this.fullscreenLoading = false
-            this.$message.success(res.message)
-            this.dialogVisible = false
-            this.getList()
-          }).catch(res => {
-            this.fullscreenLoading = false
-            this.$message.error(res.message)
-          })
+          productUpdateApi(this.productId, this.formValidate)
+            .then(async res => {
+              this.fullscreenLoading = false;
+              this.$message.success(res.message);
+              this.dialogVisible = false;
+              this.getList();
+            })
+            .catch(res => {
+              this.fullscreenLoading = false;
+              this.$message.error(res.message);
+            });
         } else {
-          return false
+          return false;
         }
-      })
+      });
     },
     onChangeGroup() {
-      this.checkboxGroup.includes('is_benefit') ? this.formValidate.is_benefit = 1 : this.formValidate.is_benefit = 0 && this.checkboxGroup.remove('is_benefit')
-      this.checkboxGroup.includes('is_best') ? this.formValidate.is_best = 1 : this.formValidate.is_best = 0 && this.checkboxGroup.remove('is_best')
-      this.checkboxGroup.includes('is_new') ? this.formValidate.is_new = 1 : this.formValidate.is_new = 0 && this.checkboxGroup.remove('is_new')
-      this.checkboxGroup.includes('is_hot') ? this.formValidate.is_hot = 1 : this.formValidate.is_hot = 0 && this.checkboxGroup.remove('is_hot')
+      this.checkboxGroup.includes('is_benefit')
+        ? (this.formValidate.is_benefit = 1)
+        : (this.formValidate.is_benefit =
+            0 && this.checkboxGroup.remove('is_benefit'));
+      this.checkboxGroup.includes('is_best')
+        ? (this.formValidate.is_best = 1)
+        : (this.formValidate.is_best =
+            0 && this.checkboxGroup.remove('is_best'));
+      this.checkboxGroup.includes('is_new')
+        ? (this.formValidate.is_new = 1)
+        : (this.formValidate.is_new = 0 && this.checkboxGroup.remove('is_new'));
+      this.checkboxGroup.includes('is_hot')
+        ? (this.formValidate.is_hot = 1)
+        : (this.formValidate.is_hot = 0 && this.checkboxGroup.remove('is_hot'));
     },
     handleClose() {
-      this.dialogVisible = false
+      this.dialogVisible = false;
     },
     // 批量下架
     batchOff() {
-      if (this.multipleSelection.length === 0) return this.$message.warning('请先选择商品')
-      this.toOff(this.OffId)
+      if (this.multipleSelection.length === 0)
+        return this.$message.warning('请先选择商品');
+      this.toOff(this.OffId);
     },
     // 下架
     toOff(id) {
@@ -384,91 +416,104 @@ export default {
         inputErrorMessage: '请输入强制下架原因',
         inputType: 'textarea',
         inputPlaceholder: '请输入强制下架原因',
-        inputValidator: (value) => {
+        inputValidator: value => {
           if (!value) {
-            return '请输入强制下架原因'
+            return '请输入强制下架原因';
           }
         }
-      }).then(({ value }) => {
-        productOffApi({ id: id, status: -2, refusal: value }).then(res => {
-          this.$message({
-            type: 'success',
-            message: '提交成功'
-          })
-          this.getLstFilterApi()
-          this.getList()
-        }).catch((res) => {
-          this.$message.error(res.message)
-        })
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '取消输入'
-        })
       })
+        .then(({ value }) => {
+          productOffApi({ id: id, status: -2, refusal: value })
+            .then(res => {
+              this.$message({
+                type: 'success',
+                message: '提交成功'
+              });
+              this.getLstFilterApi();
+              this.getList();
+            })
+            .catch(res => {
+              this.$message.error(res.message);
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消输入'
+          });
+        });
     },
     // 列表表头；
     getLstFilterApi() {
-      lstFilterApi().then(res => {
-        this.headeNum = res.data
-      }).catch(res => {
-        this.$message.error(res.message)
-      })
+      lstFilterApi()
+        .then(res => {
+          this.headeNum = res.data;
+        })
+        .catch(res => {
+          this.$message.error(res.message);
+        });
     },
     // 商户列表；
     getMerSelect() {
-      merSelectApi().then(res => {
-        this.merSelect = res.data
-      }).catch(res => {
-        this.$message.error(res.message)
-      })
+      merSelectApi()
+        .then(res => {
+          this.merSelect = res.data;
+        })
+        .catch(res => {
+          this.$message.error(res.message);
+        });
     },
     batch() {
-      if (this.multipleSelection.length === 0) return this.$message.warning('请先选择商品')
-      this.$refs.infoFrom.dialogVisible = true
-      this.isShow = false
+      if (this.multipleSelection.length === 0)
+        return this.$message.warning('请先选择商品');
+      this.$refs.infoFrom.dialogVisible = true;
+      this.isShow = false;
     },
     handleSelectionChange(val) {
-      this.multipleSelection = val
-      const data = []
-      this.multipleSelection.map((item) => {
-        data.push(item.product_id)
-      })
-      this.OffId = data
+      this.multipleSelection = val;
+      const data = [];
+      this.multipleSelection.map(item => {
+        data.push(item.product_id);
+      });
+      this.OffId = data;
     },
     toExamine(id) {
-      this.$refs.infoFrom.dialogVisible = true
-      this.isShow = true
-      this.$refs.infoFrom.getInfo(id)
+      this.$refs.infoFrom.dialogVisible = true;
+      this.isShow = true;
+      this.$refs.infoFrom.getInfo(id);
     },
     // 商户分类；
     getCategorySelect() {
-      categoryListApi().then(res => {
-        this.merCateList = res.data
-      }).catch(res => {
-        this.$message.error(res.message)
-      })
+      categoryListApi()
+        .then(res => {
+          this.merCateList = res.data;
+        })
+        .catch(res => {
+          this.$message.error(res.message);
+        });
     },
     // 列表
     getList(num) {
-      this.listLoading = true
+      this.listLoading = true;
       this.tableFrom.page = num ? num : this.tableFrom.page;
-      productLstApi(this.tableFrom).then(res => {
-        this.tableData.data = res.data.list
-        this.tableData.total = res.data.count
-        this.listLoading = false
-      }).catch(res => {
-        this.listLoading = false
-        this.$message.error(res.message)
-      })
+      productLstApi(this.tableFrom)
+        .then(res => {
+          this.tableData.data = res.data.list;
+          this.tableData.total = res.data.count;
+          this.listLoading = false;
+        })
+        .catch(res => {
+          this.listLoading = false;
+          this.$message.error(res.message);
+        });
     },
     pageChange(page) {
-      this.tableFrom.page = page
-      this.getList()
+      this.tableFrom.page = page;
+      this.getList();
     },
     handleSizeChange(val) {
-      this.tableFrom.limit = val
-      this.getList()
+      this.tableFrom.limit = val;
+      this.getList();
     }
     // 删除
     // handleDelete(id, idx) {
@@ -482,28 +527,28 @@ export default {
     //   })
     // },
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
-  .demo-table-expand {
-    font-size: 0;
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand {
+  /deep/ label {
+    width: 77px;
+    color: #99a9bf;
   }
-  .demo-table-expand {
-    /deep/ label {
-      width: 77px;
-      color: #99a9bf;
-    }
-  }
-  .demo-table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 33.33%;
-  }
-  .selWidth{
-    width: 350px !important;
-  }
-  .seachTiele{
-    line-height: 35px;
-  }
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 33.33%;
+}
+.selWidth {
+  width: 350px !important;
+}
+.seachTiele {
+  line-height: 35px;
+}
 </style>
